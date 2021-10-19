889f64f4a884e620e3ba563bb5382f053a329145
huangxiaogang <hxg_zzu@163.com>
2021-10-19 20:00:49 +0800

    openglqt5 commit

---
+++ Lesson13_BitmapFonts/Lesson13_BitmapFonts.pro
@@ -0,0 +1,28 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-07-01T11:31:25
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+target.path = /home/pi/NeHe_OpenGL_Qt5
+TARGET = Lesson13_BitmapFonts
+INSTALLS += target
+TEMPLATE = app
+INCLUDEPATH +=D:/OSG/freetype-windows-binaries-master/freetype-windows-binaries-master/include
+INCLUDEPATH +=D:/OSG/freetype-windows-binaries-master/freetype-windows-binaries-master/include/freetype
+LIBS += libfreetype libz
+
+SOURCES += main.cpp \
+    openglwindow.cpp \
+    fontwindow.cpp
+
+HEADERS  += \
+    openglwindow.h \
+    fontwindow.h
+
+RESOURCES += \
+    src.qrc
---
+++ Lesson13_BitmapFonts/fontwindow.h
@@ -0,0 +1,36 @@
+#ifndef FONTWINDOW_H
+#define FONTWINDOW_H
+
+#include <QOpenGLShaderProgram>
+#include <QApplication>
+#include <ft2build.h>
+#include <freetype/freetype.h>
+#include <freetype/config/ftheader.h>
+#include <QtMath>
+#include "openglwindow.h"
+
+class FontWindow : public OpenGLWindow
+{
+    Q_OBJECT
+public:
+    explicit FontWindow(QWindow *parent = 0);
+    ~FontWindow();
+protected:
+    void initialize();
+private:
+    void initFreeType();
+
+    void render();
+private:
+    void loadShader();
+
+    void renderText(const char *text, float x, float y, float sx, float sy);
+private:
+    QOpenGLShaderProgram *m_program;
+
+    FT_Face m_ftFace;
+    GLfloat	m_cnt1;
+    GLfloat m_cnt2;
+};
+
+#endif // FONTWINDOW_H
